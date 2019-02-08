cc5414bc3d414784c4b10cc88c772e92f7bbb1ee
shubhammittal009 <shubhammittal5114@gmail.com>
2019-02-08 12:29:55 +0530

    minor changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -321,8 +321,24 @@
 	  'autoclose': true
 	});
 
-
-
-
+	$(document).ready(function() {
+		var $chatbox = $('.chatbox'),
+				$chatboxTitle = $('.chatbox__title'),
+				$chatboxTitleClose = $('.chatbox__title__close'),
+				$chatboxCredentials = $('.chatbox__credentials');
+		$chatboxTitle.on('click', function() {
+				$chatbox.toggleClass('chatbox--tray');
+		});
+		$chatboxTitleClose.on('click', function(e) {
+				e.stopPropagation();
+				$chatbox.addClass('chatbox--closed');
+		});
+		$chatbox.on('transitionend', function() {
+				if ($chatbox.hasClass('chatbox--closed')) $chatbox.remove();
+		});
+		$chatboxCredentials.on('submit', function(e) {
+				e.preventDefault();
+				$chatbox.removeClass('chatbox--empty');
+		});
+});
 })(jQuery);
-
